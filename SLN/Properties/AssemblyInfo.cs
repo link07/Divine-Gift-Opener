a9231d9758eaf5fb5c8838168181e5a3f0a19fa8
Tyler Holinka <1685504+tholinka@users.noreply.github.com>
2015-05-10 03:01:57 -0500

    Update AssemblyInfo

--- SLN/Properties/AssemblyInfo.cs
+++ SLN/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Divine Gift Opener")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A program to click anywhere on a windows screen, repeating over a set amount of time; made for Archeage Divine Gifts")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Divine Gift Opener")]
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.4.1")]
