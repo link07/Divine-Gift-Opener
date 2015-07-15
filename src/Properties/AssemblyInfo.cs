704f94d84969d9eee2c1bc89f4b9c0e574b2363a
Tyler Holinka <1685504+tholinka@users.noreply.github.com>
2015-07-15 18:23:12 -0500

    Split into Interval Click Graphic and Interval Click Console

---
+++ src/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Interval Click Graphic by Tyler / Zafar v1.2")]
+[assembly: AssemblyDescription("Interval Click Graphic by Tyler / Zafar v1.2")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Interval Click Graphic v1.2")]
+[assembly: AssemblyCopyright("Copyright ©  Tyler / Zafar 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("818778cf-408e-4a2d-a5a2-9c49ae276f2e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
